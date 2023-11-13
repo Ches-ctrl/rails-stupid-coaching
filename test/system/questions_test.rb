e8d4b20f46f91047032250da67a8d1fd7eceb8a5
Charlie Cheesman <djnr.cheesman@gmail.com>
2023-11-13 15:29:42 +0000

    adding exercise with tests

---
+++ test/system/questions_test.rb
@@ -0,0 +1,36 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit questions_url
+  #
+  #   assert_selector "h1", text: "Questions"
+  # end
+
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "user_input", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "A question should get a silly question response from the coach" do
+    visit ask_url
+    fill_in "user_input", with: "Hello?"
+    click_on "Ask"
+
+    assert_text 'Silly question, get dressed and go to work!'
+  end
+
+  test "Has button" do
+    visit ask_url
+    # expect(page).to have_css('.btn-primary')
+    assert page.has_css?('table tr.foo')
+  end
+end
